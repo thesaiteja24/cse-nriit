cab7e4ac153942abf68e39506c1d782508edaec4
TheSaiTeja <iamsaiteja2411@gmail.com>
2024-12-17 03:45:50 +0530

    add model for course and populate db with temporary data, add TimeTablePanel Component

---
+++ backend/models/course.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema(
+  {
+    courseCode: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    shortName: {
+      type: String,
+      required: true,
+    },
+    credits: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 6,
+    },
+    type: {
+      type: String,
+      enum: ["THEORY", "LAB"],
+      required: true,
+    },
+    department: {
+      type: String,
+      required: true,
+    },
+    semester: {
+      type: String, // Updated to accept "3-1" format
+      required: true,
+    },
+    regulation: {
+      type: String,
+      required: true, // Added the regulation field
+    },
+    category: {
+      isLab: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Course = mongoose.model("Course", courseSchema);
+module.exports = Course;
---
+++ frontend/src/components/TimeTablePanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TimeTablePanel = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TimeTablePanel
