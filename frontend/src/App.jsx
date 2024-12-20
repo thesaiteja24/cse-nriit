c5be12d629d26be316072896b814663a2edf4f34
TheSaiTeja <iamsaiteja2411@gmail.com>
2024-12-20 14:56:24 +0530

    update app component to hide navigation in login and register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,18 +11,26 @@ import Register from "./components/Register";
 import LandingPage from "./components/LandingPage";
 import ViewCourses from "./components/ViewCourses";
 import ViewFaculty from "./components/ViewFaculty";
+import AssignFaculty from "./components/assignFaculty";
 function Navigation() {
   const location = useLocation();
 
   // Hide the navigation on the LandingPage
-  if (location.pathname === "/") {
+  if (
+    location.pathname === "/" ||
+    location.pathname === "/login" ||
+    location.pathname === "/register"
+  ) {
     return null; // Don't render the navigation
   }
 
   return (
     <nav className="p-2 m-2 flex flex-row justify-center">
       <div className="flex justify-evenly w-1/3">
-        <NavLink className={"bg-black rounded-full text-white px-3 py-1"} to="/">
+        <NavLink
+          className={"bg-black rounded-full text-white px-3 py-1"}
+          to="/"
+        >
           Home
         </NavLink>
         <NavLink
@@ -36,6 +44,12 @@ function Navigation() {
           to="/faculty"
         >
           Faculty
+        </NavLink>
+        <NavLink
+          className={"bg-black rounded-full text-white px-3 py-1 "}
+          to="/assignFaculty"
+        >
+          Assign
         </NavLink>
       </div>
     </nav>
@@ -53,6 +67,7 @@ export default function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/courses" element={<ViewCourses />} />
           <Route path="/faculty" element={<ViewFaculty />} />
+          <Route path="/assignFaculty" element={<AssignFaculty />}></Route>
         </Routes>
       </div>
     </Router>
