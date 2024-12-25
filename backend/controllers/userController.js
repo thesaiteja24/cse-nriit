eda80ce888c50f41207718a96b4ec1d4921d8658
TheSaiTeja <iamsaiteja2411@gmail.com>
2024-12-25 20:40:05 +0530

    feat(auth): Implement session-based authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -62,11 +62,42 @@ exports.loginUser = async (req, res, next) => {
         if (err) {
           return res.status(500).json({ message: "Error during login" });
         }
-        return res.status(200).json({ message: "Login successful", user });
+        req.session.userId = user._id;
+        return res.status(200).json({
+          user: {
+            id: user._id,
+            email: user.email,
+            name: user.name,
+          },
+          message: "Login successful",
+          user,
+        });
       });
     })(req, res, next);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+exports.logout = async (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ message: "Could not log out" });
+    }
+    res.clearCookie("connect.sid");
+    return res.json({ message: "Logged out successfully" });
+  });
+};
+
+exports.getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.session.userId).select("-password");
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
   }
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/userController");
+const auth = require("../middlewares/auth");
+const {
+  registerUser,
+  loginUser,
+  logout,
+  getMe,
+} = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
+router.post("/logout", logout);
+router.get("/getuser", auth, getMe);
 module.exports = router;
